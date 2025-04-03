ac1200f8440e473a660a4b3fbefb5b93fcf3751e
Nickcau1203 <158166129+Nickcau1203@users.noreply.github.com>
2025-04-03 16:03:05 -0300

    feat: quase termino das indexs

---
+++ components/InteractionBar/index.jsx
@@ -0,0 +1,26 @@
+import style from './InteractionBar.module.css';
+
+const InteractionBar = () => {
+    return (
+        <div className={styles.interactionBar}>
+                      <div className={styles.interactionButton}>
+                        <span>👍</span>
+                        <span>{meme.likes}</span>
+                      </div>
+                      <div className={styles.interactionButton}>
+                        <span>💬</span>
+                        <span>{meme.comments}</span>
+                      </div>
+                      <div className={styles.interactionButton}>
+                        <span>🔄</span>
+                        <span>Share</span>
+                      </div>
+                      <div className={styles.interactionButton}>
+                        <span>🔖</span>
+                        <span>Save</span>
+                      </div>
+                    </div>
+    );
+};
+
+export default InteractionBar;
